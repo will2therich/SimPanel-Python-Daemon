6255be4b05e367c1a0f3eb5ecdf78282941a7226
William Rich <will@servers4all.co.uk>
2018-06-07 12:54:02 +0100

    More work on the python daemon to make things smoother with authentication

--- Sockets.py
+++ Sockets.py
@@ -1,5 +1,22 @@
+#   _________.__                                   .__
+#  /   _____/|__| _____ ___________    ____   ____ |  |
+#  \_____  \ |  |/     \\____ \__  \  /    \_/ __ \|  |
+#  /        \|  |  Y Y  \  |_> > __ \|   |  \  ___/|  |__
+# /_______  /|__|__|_|  /   __(____  /___|  /\___  >____/
+#         \/          \/|__|       \/     \/     \/
+# ________
+# \______ \ _____    ____   _____   ____   ____
+#  |    |  \\__  \ _/ __ \ /     \ /  _ \ /    \
+#  |    `   \/ __ \\  ___/|  Y Y  (  <_> )   |  \
+# /_______  (____  /\___  >__|_|  /\____/|___|  /
+#         \/     \/     \/      \/            \/
+#
+#________________________________________________________________________________________________________________________
+#                       DO NOT EDIT
+#
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket, SimpleSSLWebSocketServer
 from Master import handlecommand
+from config import *
 
 clients = []
 
@@ -13,7 +30,6 @@ class DaemonWebsocketService(WebSocket):
             client.sendMessage(self.address[0] + u' - connected')
 
     def handleClosed(self):
-        print(self.address, 'closed')
         clients.remove(self)
         for client in clients:
             client.sendMessage(self.address[0] + u' - disconnected')
@@ -26,11 +42,22 @@ class DaemonWebsocketService(WebSocket):
             seperatedData = data.split(':')
             returnMessage = handlecommand(seperatedData[0], seperatedData[1], self)
 
+            # Handling closing via command
+            if returnMessage == 'close':
+                print(self.address , "Disconnected")
+                self.sendMessage(u'Returned:' + returnMessage)
+                self.handleClosed(self)
+
             self.sendMessage(u'Returned:' +returnMessage)
 
 
-# server = SimpleWebSocketServer('', 9005, DaemonWebsocketService)
-server = SimpleSSLWebSocketServer('', 9005, DaemonWebsocketService, './ssl-cert-snakeoil.pem',
-                                  './ssl-cert-snakeoil.key')
+# Update the certificate files in the config
+# In order to use SSL you'll need to link the ip to a domain and SSL the domain.
+if sslEnable:
+    server = SimpleSSLWebSocketServer(ipToListenOn, portToListenOn, DaemonWebsocketService, sslCert,  sslKey)
+    print("Websockets Live and listening on port: " + str(portToListenOn))
+else:
+    server = SimpleWebSocketServer(ipToListenOn, portToListenOn, DaemonWebsocketService)
+    print("Websockets Live and listening on port: " + str(portToListenOn))
 
 server.serveforever()
--- config.py
+++ config.py
@@ -1,4 +1,39 @@
+
+#   _________.__                                   .__
+#  /   _____/|__| _____ ___________    ____   ____ |  |
+#  \_____  \ |  |/     \\____ \__  \  /    \_/ __ \|  |
+#  /        \|  |  Y Y  \  |_> > __ \|   |  \  ___/|  |__
+# /_______  /|__|__|_|  /   __(____  /___|  /\___  >____/
+#         \/          \/|__|       \/     \/     \/
+# ________
+# \______ \ _____    ____   _____   ____   ____
+#  |    |  \\__  \ _/ __ \ /     \ /  _ \ /    \
+#  |    `   \/ __ \\  ___/|  Y Y  (  <_> )   |  \
+# /_______  (____  /\___  >__|_|  /\____/|___|  /
+#         \/     \/     \/      \/            \/
+#
+#________________________________________________________________________________________________________________________
+# _________                _____.__
+# \_   ___ \  ____   _____/ ____\__| ____
+# /    \  \/ /  _ \ /    \   __\|  |/ ___\
+# \     \___(  <_> )   |  \  |  |  / /_/  >
+#  \______  /\____/|___|  /__|  |__\___  /
+#         \/            \/        /_____/
+#
+# Created by Servers4All
+
+# Panel Details including AUTH key (Generated when creating a server)
 authKey = '21380129380haksjhdkjassd8934'
 panelDomain = 'simpanel.local'
-panelApiKey = ''
+panelApiKey = '' # Not Currently In Use
 enviroment = 'dev'
+
+# Listening Config
+ipToListenOn = '127.0.0.1'
+portToListenOn = 9005
+domainToListenOn = ''
+
+# SSL config
+sslEnable = True
+sslCert = './ssl-cert-snakeoil.pem'
+sslKey = './ssl-cert-snakeoil.key'
